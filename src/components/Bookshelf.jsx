06c2305642fa9478c22389a4d9490cbe45614bb2
Gerson Dias <gersondias@outlook.com>
2018-02-15 17:10:44 -0200

    Feat: Search page fully functional

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -13,46 +13,51 @@ class Bookshelf extends React.Component {
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
         <div className="bookshelf-books">
-          <ol className="books-grid">
-            {this.props.books.map(book => {
-              return (
-                <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div
-                        className="book-cover"
-                        style={{
-                          width: 128,
-                          height: 193,
-                          backgroundImage: `url("${book.coverUrl}"`
-                        }}
-                      />
-                      <div className="book-shelf-changer">
-                        <select
-                          onChange={e => {
-                            this.props.onChangeShelf(book, e.target.value);
+          {this.props.books && (
+            <ol className="books-grid">
+              {this.props.books.map(book => {
+                return (
+                  <li key={book.id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div
+                          className="book-cover"
+                          style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url("${
+                              book.imageLinks.smallThumbnail
+                            }"`
                           }}
-                          defaultValue={book.shelfTitle}
-                        >
-                          <option value="none" disabled>
-                            Move to...
-                          </option>
-                          <option value="currentlyReading">
-                            Currently Reading
-                          </option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
+                        />
+                        <div className="book-shelf-changer">
+                          <select
+                            onChange={e => {
+                              this.props.onChangeShelf(book, e.target.value);
+                            }}
+                            defaultValue={book.shelf || 'none'}
+                          >
+                            <option value="none" disabled>
+                              Move to...
+                            </option>
+                            <option value="currentlyReading">
+                              Currently Reading
+                            </option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
                       </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors}</div>
                     </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
-                  </div>
-                </li>
-              );
-            })}
-          </ol>
+                  </li>
+                );
+              })}
+            </ol>
+          )}
+          {!this.props.books.length && <h5>No books to show</h5>}
         </div>
       </div>
     );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,30 +1,90 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { search, update, getAll } from '../services/BooksAPI';
+import Bookshelf from './Bookshelf';
 
-const Search = () => {
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <Link to="/" className="close-search">
-          Close
-        </Link>
-        <div className="search-books-input-wrapper">
-          {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+class Search extends React.Component {
+  state = {
+    books: []
+  };
 
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                  */}
-          <input type="text" placeholder="Search by title or author" />
+  currentlyBooks = { books: [] };
+
+  componentDidMount() {
+    // should getAll books to be able to sync shelf state.
+    // this should be done because search API doesn't provide the shelf property
+    getAll()
+      .then(books => (this.currentlyBooks = books))
+      .catch(err => {
+        alert('Unable to get currently books on a shelf.');
+        console.log(err);
+      });
+  }
+
+  handleChange = ev => {
+    if (ev.currentTarget.value.length >= 3) {
+      search(ev.currentTarget.value)
+        .then(books => {
+          if (books.error) {
+            alert('Unable to process your request: ' + books.error);
+            return;
+          }
+
+          let enhancedBooks = books.map(book => {
+            let bookInAShelf = this.currentlyBooks.find(
+              bookInAShelf => book.id === bookInAShelf.id
+            );
+            if (bookInAShelf)
+              return Object.assign({}, book, {
+                shelf: bookInAShelf.shelf
+              });
+
+            return book;
+          });
+
+          this.setState({ books: enhancedBooks });
+        })
+        .catch(err => console.log('error ' + err));
+    }
+  };
+
+  changeShelf = (book, shelf) => {
+    update(book, shelf);
+  };
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            {/*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={this.handleChange}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <Bookshelf
+            shelfTitle="Search results"
+            books={this.state.books}
+            onChangeShelf={this.changeShelf}
+          />
         </div>
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Search;
