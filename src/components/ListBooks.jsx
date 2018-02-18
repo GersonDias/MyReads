ae31ca4ebf9a7c13b14c730a12a4d8f930c54a70
Gerson Dias <contato@gersondias.net>
2018-02-18 12:42:24 -0300

    reafactor: Initial Shelfs as array

--- src/components/ListBooks.jsx
+++ src/components/ListBooks.jsx
@@ -6,14 +6,32 @@ import PropTypes from 'prop-types';
 class ListBooks extends React.Component {
   static propTypes = {
     books: PropTypes.array.isRequired,
-    onBookChanged: PropTypes.func.isRequired
+    onBookChanged: PropTypes.func.isRequired,
   };
 
-  getBooksForShelf = shelfTitle => {
-    return this.props.books.filter(book => book.shelf === shelfTitle);
+  getBooksForShelf = shelfId => {
+    return this.props.books.filter(book => book.shelf === shelfId);
   };
 
   render() {
+    const initialShelfs = [
+      {
+        id: 'currentlyReading',
+        shelfTitle: 'Currently Reading',
+        books: this.getBooksForShelf('currentlyReading'),
+      },
+      {
+        id: 'wantToRead',
+        shelfTitle: 'Want to Read',
+        books: this.getBooksForShelf('wantToRead'),
+      },
+      {
+        id: 'read',
+        shelfTitle: 'Read',
+        books: this.getBooksForShelf('read'),
+      },
+    ];
+
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -21,21 +39,16 @@ class ListBooks extends React.Component {
         </div>
         <div className="list-books-content">
           <div>
-            <Bookshelf
-              shelfTitle="Currently Reading"
-              books={this.getBooksForShelf('currentlyReading')}
-              onChangeShelf={this.props.onBookChanged}
-            />
-            <Bookshelf
-              shelfTitle="Want to Read"
-              books={this.getBooksForShelf('wantToRead')}
-              onChangeShelf={this.props.onBookChanged}
-            />
-            <Bookshelf
-              shelfTitle="Read"
-              books={this.getBooksForShelf('read')}
-              onChangeShelf={this.props.onBookChanged}
-            />
+            {initialShelfs.map(shelf => {
+              return (
+                <Bookshelf
+                  key={shelf.id}
+                  shelfTitle={shelf.shelfTitle}
+                  books={shelf.books}
+                  onChangeShelf={this.props.onBookChanged}
+                />
+              );
+            })}
             <div className="open-search">
               <Link to="/search">Add a book</Link>
             </div>
